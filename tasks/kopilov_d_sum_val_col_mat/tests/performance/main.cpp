c6c70c41353084eb5860395726b2d8529aba7506
TiClass322 <dana.kopilov@gmail.com>
2025-11-21 14:02:59 +0300

    4000x4000 matrix perf_test

--- tasks/kopilov_d_sum_val_col_mat/tests/performance/main.cpp
+++ tasks/kopilov_d_sum_val_col_mat/tests/performance/main.cpp
@@ -9,7 +9,7 @@ namespace kopilov_d_sum_val_col_mat {
 
 class KopilovDSumValColMatPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
-  int size_ = 100000;
+  int size_ = 4000;
   InType input_data_;
   OutType expected_;
 
