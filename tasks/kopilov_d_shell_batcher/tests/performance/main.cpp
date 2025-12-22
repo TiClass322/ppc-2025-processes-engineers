c45e1b3a8fda469e35b320c16866480565213bac
TiClass322 <dana.kopilov@gmail.com>
2025-12-22 19:24:39 +0300

    kopilov_d_shell_merge

---
+++ tasks/kopilov_d_shell_batcher/tests/performance/main.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+
+#include <cstddef>
+#include <utility>
+#include <vector>
+
+#include "kopilov_d_shell_batcher/common/include/common.hpp"
+#include "kopilov_d_shell_batcher/mpi/include/ops_mpi.hpp"
+#include "kopilov_d_shell_batcher/seq/include/ops_seq.hpp"
+#include "util/include/perf_test_util.hpp"
+
+namespace kopilov_d_shell_batcher {
+
+namespace {
+
+void ShellSort(InType &vec) {
+  const std::size_t n = vec.size();
+  for (std::size_t gap = n / 2; gap > 0; gap /= 2) {
+    for (std::size_t i = gap; i < n; ++i) {
+      const int tmp = vec[i];
+      std::size_t j = i;
+      while (j >= gap && vec[j - gap] > tmp) {
+        vec[j] = vec[j - gap];
+        j -= gap;
+      }
+      vec[j] = tmp;
+    }
+  }
+}
+
+}  // namespace
+
+class KopilovDShellBatcherRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+ public:
+  void SetUp() override {
+    constexpr std::size_t kSize = 200000;
+    input_data_.resize(kSize);
+
+    unsigned int x = 17;
+    for (std::size_t i = 0; i < kSize; ++i) {
+      x = (x * 1103515245) + 12345;
+      input_data_[i] = static_cast<int>(x);
+    }
+
+    expected_ = input_data_;
+    ShellSort(expected_);
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return output_data == expected_;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+ private:
+  InType input_data_;  // PrivateMemberSuffix: _
+  OutType expected_;   // PrivateMemberSuffix: _
+};
+
+TEST_P(KopilovDShellBatcherRunPerfTestProcesses, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
+
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, KopilovDShellBatcherMPI, KopilovDShellBatcherSEQ>(
+    PPC_SETTINGS_kopilov_d_shell_batcher);
+
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+const auto kPerfTestName = KopilovDShellBatcherRunPerfTestProcesses::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, KopilovDShellBatcherRunPerfTestProcesses, kGtestValues, kPerfTestName);
+
+}  // namespace kopilov_d_shell_batcher
